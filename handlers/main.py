e3f4a3e221c6da8c6dbf9d98daee258a3a1dc7d9
tsteevy <twitter@kuechenscha.be>
2016-12-07 21:12:37 +0100

    move hashing of password to async thread

--- handlers/main.py
+++ handlers/main.py
@@ -1,6 +1,7 @@
 from handlers.base import BaseHandler
-import logging
 import bcrypt
+import logging
+import threading
 
 logger = logging.getLogger()
 
@@ -13,6 +14,13 @@ class MainHandler(BaseHandler):
         unhashed_password = self.get_argument("inputPassword").encode('utf-8')
         username = self.get_argument("inputUser")
         email = self.get_argument("inputEmail")
-        hashed_password = bcrypt.hashpw(unhashed_password, bcrypt.gensalt())
-        logger.info("%s; %s; %s", username, hashed_password, email)
+
+        # move blocking hashing of password to own thread
+        thread = threading.Thread(target=self.hash_password, args=(username, unhashed_password, email))
+        thread.start()
+
         self.render("home.html")
+
+    def hash_password(self, username, password, email):
+        hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+        logger.info("%s; %s; %s", username, hashed_password, email)
